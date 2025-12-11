6a82c89b70d4ca6176fb345774162bdc74f10bfc
Deepak Vats <deepakvats123123@gmail.com>
2025-12-11 14:19:02 +0530

    Automatic Inferencing & Annotation

---
+++ src/typesInTs.ts
@@ -0,0 +1,10 @@
+// Automatic Infrenching (it will take automatically can Not change later)
+let name = "Deepak Vats"; // string
+let age = 25; // number
+let isMarried = false; // boolean
+
+// Annotaion (we can assign)
+
+let myName: string = "Deepak"
+let myAge: Number = 25;
+let marritalStatus: boolean = false
